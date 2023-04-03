bf11d212fbb247bf4c8d12082ea9709dd4455849
Aleix Pol <aleixpol@kde.org>
2023-04-03 02:52:24 +0200

    pk: Simplify implementation to check if discover is running from the notifier

--- notifier/DiscoverNotifier.cpp
+++ notifier/DiscoverNotifier.cpp
@@ -27,6 +27,7 @@
 #include "../libdiscover/utils.h"
 #include "updatessettings.h"
 #include <chrono>
+#include <qdbusconnectioninterface.h>
 
 using namespace std::chrono_literals;
 
@@ -121,13 +122,7 @@ bool DiscoverNotifier::notifyAboutUpdates() const
     m_settings->setLastNotificationTime(QDateTime::currentDateTimeUtc());
     m_settings->save();
 
-    auto method = QDBusMessage::createMethodCall(QStringLiteral("org.kde.discover"),
-                                                 QStringLiteral("/"),
-                                                 QStringLiteral("org.freedesktop.DBus.Peer"),
-                                                 QStringLiteral("Ping"));
-    auto call = QDBusConnection::sessionBus().asyncCall(method);
-    call.waitForFinished();
-    if (call.isValid()) {
+    if (QDBusConnection::sessionBus().interface()->isServiceRegistered(QStringLiteral("org.kde.discover"))) {
         return false;
     }
     return true;
